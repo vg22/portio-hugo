fa5f47763171ed912bda2ca7a9df6a1481ac5d5e
vg22 <vgdeveops22@gmail.com>
2022-06-06 15:55:14 -0400

    update

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -1,17 +1,17 @@
-window.addEventListener("DOMContentLoaded", function () {
-  var form = document.getElementById("contact-form");
-  var button = document.getElementById("contact-form-button");
-  var status = document.getElementById("contact-form-status");
+// window.addEventListener("DOMContentLoaded", function () {
+//   var form = document.getElementById("contact-form");
+//   var button = document.getElementById("contact-form-button");
+//   var status = document.getElementById("contact-form-status");
 
-  function success() {
-    form.reset();
-    button.style = "display: none ";
-    status.innerHTML = "Thanks! Contact form is submitted successfully.";
-  }
+//   function success() {
+//     form.reset();
+//     button.style = "display: none ";
+//     status.innerHTML = "Thanks! Contact form is submitted successfully.";
+//   }
 
-  function error() {
-    status.innerHTML = "Oops! There was a problem.";
-  }
+//   function error() {
+//     status.innerHTML = "Oops! There was a problem.";
+//   }
 
   // handle the form submission event
   if (form != null) {
